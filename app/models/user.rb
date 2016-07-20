15f80cae1913fafaf9bea8de2080c8071e8a963b
Iran <iranthau@gmail.com>
2016-07-20 18:54:49 +1000

    Add articles user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :articles
+
+  before_save { self.email = email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :username,
-            presence: true,
+  validates :username, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
-  validates :email,
-            presence: true,
+  validates :email, presence: true,
             uniqueness: { case_sensitive: false },
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
