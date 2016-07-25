36eb96b40faaf618b58d5b254b46bb62d24a9ad4
Iran R <iranthau@gmail.com>
2016-07-25 23:52:15 +1000

    Add user sign up capabilities

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20160720103054) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
