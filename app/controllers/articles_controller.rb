716161de19726e31c59b2f99260ce6aeda5bcef4
Iran <iranthau@gmail.com>
2016-09-30 02:19:19 +1000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,9 +14,9 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first #temp code
+    @article.user = current_user
     if @article.save
-      flash[:success] = "Article created successfully"
+      flash[:success] = 'Article created successfully'
       redirect_to article_path(@article)
     else
       render :new
@@ -28,7 +28,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:danger] = "Article was deleted"
+    flash[:danger] = 'Article was deleted'
     redirect_to articles_path
   end
 
@@ -37,7 +37,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:success] = "Article successfully updated"
+      flash[:success] = 'Article successfully updated'
       redirect_to article_path(@article)
     else
       render :edit
