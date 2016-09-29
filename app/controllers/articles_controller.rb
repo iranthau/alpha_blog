6a743743d8a619edfc9841bf5739df9f17a403b9
Iran <iranthau@gmail.com>
2016-09-30 02:19:19 +1000

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
+# ArticlesController
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -21,7 +24,6 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def destroy
@@ -31,7 +33,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
@@ -51,5 +52,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = 'You can edit or delete your own articles'
+      redirect_to(root_path)
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "Logged in successfully"
+      flash[:success] = 'Logged in successfully'
       redirect_to user_path(user)
     else
-      flash.now[:danger] = "Incorecct log in details"
+      flash.now[:danger] = 'Incorecct log in details'
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "Logged out"
+    flash[:success] = 'Logged out'
     redirect_to root_path
   end
 end
