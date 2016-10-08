f3c37a15a4aacbe46a8f3e57c0ee89df702fc853
Iran <iranthau@gmail.com>
2016-10-08 23:28:48 +1100

    Add admin requirement to create categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 # Create Categories Test
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: 'books')
+    @category = Category.create(name: 'sports')
     @category_2 = Category.create(name: 'programming')
   end
 
   test 'should show categories listing' do
     get categories_path
     assert_template 'categories/index'
-    assert_select 'a[href=?]', categories_path(@category), text: @category.name
-    assert_select 'a[href=?]', categories_path(@category_2), text: @category_2.name
+    assert_select 'a[href=?]', category_path(@category), text: @category.name
+    assert_select 'a[href=?]', category_path(@category_2), text: @category_2.name
   end
 end
